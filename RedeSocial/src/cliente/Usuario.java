77b7e504b7ade9e427461cbc18b6dffb3b77a635
DStalkerBR <jhon89araujo@gmail.com>
2023-11-14 14:45:16 -0600

    Adiciona pontuacao e quantidade postagens

--- RedeSocial/src/cliente/Usuario.java
+++ RedeSocial/src/cliente/Usuario.java
@@ -8,6 +8,8 @@ public class Usuario {
 	private String email;
 	private String nacionalidade;
 	private ArrayList<String> postagens;
+	private int quantidadeDePostagens;
+	private int pontuacao;
 	
 	public Usuario(String nome, String email, String nacionalidade) {
 		super();
@@ -15,34 +17,48 @@ public class Usuario {
 		this.email = email;
 		this.nacionalidade = nacionalidade;
 		this.postagens = new ArrayList<String>();
+		this.quantidadeDePostagens = 0;
+		this.pontuacao = 0;
 	}
 	
 	public static void main(String[] args) {
 		Usuario novoUsuario;
 		Scanner entrada = new Scanner(System.in);
 		String nome, email, nacionalidade, postagem;
+		
 		System.out.println("Digite o nome:");
 		nome = entrada.nextLine();
+		
 		System.out.println("Digite o email:");
 		email = entrada.nextLine();
+		
 		System.out.println("Digite a nacionalidade:");
 		nacionalidade = entrada.nextLine();
+		
 		novoUsuario = new Usuario(nome, email, nacionalidade);
+		
 		System.out.println("Crie uma nova postagem:");
 		postagem = entrada.nextLine();
 		novoUsuario.adicionaPostagem(postagem);
+		novoUsuario.alteraPontuacao(postagem.length());
+		
 		System.out.println("Crie uma nova postagem:");
 		postagem = entrada.nextLine();
 		novoUsuario.adicionaPostagem(postagem);
+		novoUsuario.alteraPontuacao(postagem.length());
+		
 		System.out.println("Crie uma nova postagem:");
 		postagem = entrada.nextLine();
 		novoUsuario.adicionaPostagem(postagem);
+		novoUsuario.alteraPontuacao(postagem.length());
 		
 		System.out.println("Dados do usuário: ");
-		System.out.println(novoUsuario.getNome());
-		System.out.println(novoUsuario.getEmail());
-		System.out.println(novoUsuario.getNacionalidade());
-		System.out.println("Postagens do usuário: ");
+		System.out.println("Nome: " + novoUsuario.getNome());
+		System.out.println("Email: " + novoUsuario.getEmail());
+		System.out.println("Nacionalidade: " + novoUsuario.getNacionalidade());
+		System.out.println("Quantidade de Postagens: " + novoUsuario.getQuantidadeDePostagens());
+		System.out.println("Pontuação: " + novoUsuario.getPontuacao() );
+		System.out.println("\nPostagens do usuário: ");
 		for (String post : novoUsuario.getPostagens()) {
 			System.out.println(post);
 		}
@@ -71,6 +87,7 @@ public class Usuario {
 	
 	public void adicionaPostagem(String post) {
 		postagens.add(post);
+		this.quantidadeDePostagens += 1;
 	}
 
 	public ArrayList<String> getPostagens() {
@@ -81,6 +98,19 @@ public class Usuario {
 		this.postagens = postagens;
 	}
 	
+	public int getQuantidadeDePostagens() {
+		return quantidadeDePostagens;
+	}
+	
+	public void alteraPontuacao(int delta) {
+		if (pontuacao + delta > 0) {
+			this.pontuacao += delta;
+		}
+	}
+	
+	public int getPontuacao() {
+		return pontuacao;
+	}
 	
 	
 }
